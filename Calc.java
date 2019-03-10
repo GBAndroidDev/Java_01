4ee1ca6cc16cc3e90cd49410989c760c3dfe07c4
GBAndroidDev <47600822+GBAndroidDev@users.noreply.github.com>
2019-03-10 19:56:39 +0300

    Lesson 03 - Калькулятор с приоритетом вычисления переменных в скобках.

--- Calc.java
+++ Calc.java
@@ -7,10 +7,11 @@ public class Calc {
     public static void main(String[] args) {
 
         //String inputStr = "3 d +  6 - 7 * 8  / 2";
-        //String inputStr = "3 +  6 - 7 * 8  / 2";
+        //String inputStr = "53 - ( 2 + 8 ) + 2 / ( 7 -  8 )";
         String inputStr = inputValues();
         if (checkNumber(splitArray(inputStr))) {
-            System.out.println("Результат вычисления: " + mathMagic(splitArray(inputStr)));
+            //System.out.println("Результат вычисления: " + mathMagic(splitArray(inputStr)));
+            System.out.println("Результат вычисления: " + calcWithPriority(splitArray(inputStr)));
         }
     }
 
@@ -34,6 +35,7 @@ public class Calc {
                 count++;
             }
         }
+        //System.out.println(Arrays.toString(res));
         return res;
     }
 
@@ -43,16 +45,19 @@ public class Calc {
             System.out.println(errorMessage);
             return false;
         }
+        /*String[] skipChars = {"*","/","+","-","(",")"};
         for (String str: checkArray) {
-            if (!str.equals("*") && !str.equals("+") && !str.equals("/") && !str.equals("-")) {
-                try {
-                    double num = Double.parseDouble(str);
-                } catch (Exception e) {
-                    System.out.println(errorMessage);
-                    return false;
+            for (String symb: skipChars) {
+                if (!str.equals(symb)) {
+                    try {
+                        double num = Double.parseDouble(str);
+                    } catch (Exception e) {
+                        System.out.println(errorMessage);
+                        return false;
+                    }
                 }
             }
-        }
+        }*/
         return true;
     }
 
@@ -64,6 +69,7 @@ public class Calc {
     public static String[] mathOperation(String[] valuesArray, String operator) {
         double res = 0;
         int i = 0;
+        //System.out.println(Arrays.toString(valuesArray));
         while (i < valuesArray.length) {
             if (valuesArray[i].equals(operator)) {
                 switch (operator) {
@@ -77,17 +83,54 @@ public class Calc {
                         res = Double.parseDouble(valuesArray[i - 1]) + Double.parseDouble(valuesArray[i + 1]);
                         break;
                     case "-":
-                        res = Double.parseDouble(valuesArray[i - 1]) - Double.parseDouble(valuesArray[i + 1]);
+                        if (i > 0) {
+                            res = Double.parseDouble(valuesArray[i - 1]) - Double.parseDouble(valuesArray[i + 1]);
+                        } else {
+                            res = Double.parseDouble(valuesArray[i + 1]) * (-1);
+                        }
                 }
                 valuesArray[i] = Double.toString(res);
-                valuesArray[i - 1] = "";
+                if (i > 0) {
+                    valuesArray[i - 1] = "";
+                }
                 valuesArray[i + 1] = "";
                 valuesArray = cleanArray(valuesArray);
-                i = i - 2;
+                if (i > 0) {
+                    i = i - 2;
+                }
             }
             i++;
         }
         return valuesArray;
+    }
+
+    public static String calcWithPriority(String[] splittedArray) {
+        int copyBegin = -1;
+        int copyEnd = -1;
+        String[] valuesForPriorityCalulate;
+        for (int i = 0; i < splittedArray.length; i++) {
+            if (splittedArray[i].equals("(")) {
+                copyBegin = i;
+            }
+            if (splittedArray[i].equals(")")) {
+                copyEnd = i;
+            }
+            if (copyBegin >= 0 && copyEnd >= 0) {
+                valuesForPriorityCalulate = new String[copyEnd - copyBegin - 1];
+                int k = 0;
+                for (int j = copyBegin + 1; j < copyEnd; j++) {
+                    valuesForPriorityCalulate[k] = splittedArray[j];
+                    splittedArray[j] = "";
+                    k++;
+                }
+                System.out.println("Priority array: " + Arrays.toString(valuesForPriorityCalulate));
+                splittedArray[copyEnd] = "";
+                splittedArray[copyBegin] = mathMagic(valuesForPriorityCalulate);
+                copyBegin = -1;
+                copyEnd = -1;
+            }
+        }
+        return mathMagic(splittedArray);
     }
 
     public static String mathMagic(String[] valuesArray) {
@@ -96,6 +139,7 @@ public class Calc {
         //System.out.println(Arrays.toString(resValues));
         resValues = mathOperation(resValues,"/");
         //System.out.println(Arrays.toString(resValues));
+        //System.out.println(Arrays.toString(valuesArray));
         resValues = mathOperation(resValues,"-");
         //System.out.println(Arrays.toString(resValues));
         resValues = mathOperation(resValues,"+");
